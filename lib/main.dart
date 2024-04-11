26cd73f7d5ecdae74b8d764a4cd0009a42a166e3
Fazil <mfazilkp7@gmail.com>
2024-04-11 22:13:42 +0530

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ Future main() async {
         
 
 
-
         
         ChangeNotifierProvider(create: (_) => MyData()),
         ChangeNotifierProvider(create: (context) => StudentModelProvider()),
